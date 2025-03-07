843540bb6de3c50d42bafc4929f1253697b4d505
Ayan <aabbas@ualberta.ca>
2025-03-07 15:28:40 -0700

    QUICK EDA

--- sentiment.py
+++ sentiment.py
@@ -10,3 +10,13 @@ import nltk
 df = pd.read_csv("/Users/ayaan/Downloads/data-amazon/Reviews.csv")
 
 print(df.head())
+
+# print(df.shape)
+
+#EDA
+ax = df['Score'].value_counts().sort_index() \
+    .plot(kind='bar',
+          title='Count of Reviews by Stars',
+          figsize=(10, 5))
+ax.set_xlabel('Review Stars')
+plt.show()
